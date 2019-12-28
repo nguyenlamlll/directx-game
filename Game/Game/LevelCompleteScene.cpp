81504d724ddf6f2e1bf4760edd6378e849b2a8b8
VyVy IT <38434899+lehoangvyvy997@users.noreply.github.com>
2019-12-28 07:00:57 +0700

    Added Aladdin and Abu to LevelCompleteScene (#64)
    Co-authored-by: Lam Le <15520405@gm.uit.edu.vn>

--- Game/Game/LevelCompleteScene.cpp
+++ Game/Game/LevelCompleteScene.cpp
@@ -4,13 +4,22 @@
 
 LevelCompleteScene::LevelCompleteScene(Scene* nextScene)
 {
+	pXM = Global::GetInstance()->g_ScreenWidth / 2 + 120;
+	pYM = Global::GetInstance()->g_ScreenHeight / 2 + 140;
+	pXA = pXM + 50;
+	pYA = pYM - 10;
 	m_levelComplete = new Sprite(L"Resources/title.png");
 	m_levelComplete->setPositionX(Global::GetInstance()->g_ScreenWidth / 2);
 	m_levelComplete->setPositionY(Global::GetInstance()->g_ScreenHeight / 2 + 40);
+	m_monkeyMoving = new Animation(L"Resources/AladdinState/PNG/monkey-moving-complete_8.png", 8, 1, 8, true, 40.f);
+	m_monkeyMoving->setPositionX(pXM);
+	m_monkeyMoving->setPositionY(pYM);
+	m_aladdinMoving = new Animation(L"Resources/AladdinState/PNG/AladdinWin_8.png", 8, 1, 8, true, 60.f);
+	m_aladdinMoving->setPositionX(pXA);
+	m_aladdinMoving->setPositionY(pYA);
 	RECT rect;
 	SetRect(&rect, (int)0, (int)100, (int)322, (int)150 + 110);
 	m_levelComplete->setSourceRect(rect);
-
 	m_nextScene = nextScene;
 
 	start = GetTickCount();
@@ -23,6 +32,14 @@ void LevelCompleteScene::initialize()
 
 LevelCompleteScene::~LevelCompleteScene()
 {
+}
+
+void LevelCompleteScene::ReleaseAll()
+{
+	delete m_levelComplete;
+	delete m_monkeyMoving;
+	delete m_aladdinMoving;
+	Sound::getInstance()->stop(SoundNames::LEVEL_COMPLETE_SOUD);
 }
 
 void LevelCompleteScene::Update(float deltaTime)
@@ -40,11 +57,25 @@ void LevelCompleteScene::Update(float deltaTime)
 		SceneManager::getInstance()->changeScene(m_nextScene);
 		return;
 	}
+	m_monkeyMoving->Update(deltaTime);
+	if(pXM>0)
+		pXM -= 2.5f;
+	m_monkeyMoving->setPositionX(pXM);
+	m_monkeyMoving->setPositionY(pYM);
+
+	m_aladdinMoving->Update(deltaTime);
+	if(pXA > 0)
+		pXA -= 2.5f;
+	m_aladdinMoving->setPositionX(pXA);
+	m_aladdinMoving->setPositionY(pYA);
+
 }
 
 void LevelCompleteScene::Draw()
 {
 	m_levelComplete->DrawOnScreen();
+	m_monkeyMoving->DrawOnScreen();
+	m_aladdinMoving->DrawOnScreen();
 }
 
 void LevelCompleteScene::OnKeyDown(int keyCode)
@@ -54,10 +85,3 @@ void LevelCompleteScene::OnKeyDown(int keyCode)
 void LevelCompleteScene::OnKeyUp(int keyCode)
 {
 }
-
-void LevelCompleteScene::ReleaseAll()
-{
-	delete m_levelComplete;
-	Sound::getInstance()->stop(SoundNames::LEVEL_COMPLETE_SOUD);
-}
-
--- Game/Game/LevelCompleteScene.h
+++ Game/Game/LevelCompleteScene.h
@@ -3,6 +3,7 @@
 
 #include "Sprite.h"
 #include "Text.h"
+#include "Animation.h"
 
 #include "KeyboardInput.h"
 
@@ -17,8 +18,10 @@ private:
 	DWORD start;
 	bool m_isAnimationFinished{ false };
 	Sprite* m_levelComplete;
-
+	Animation* m_aladdinMoving;
+	Animation* m_monkeyMoving;
 	Scene* m_nextScene;
+	float pXM, pYM, pXA, pYA;
 
 public:
 	LevelCompleteScene(Scene* nextScene);
